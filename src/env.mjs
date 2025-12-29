eb0cd9e0d5675ee66578d7bb400f1ce6b5cd1777
kami gerami <kami.gerami@gmail.com>
2025-12-29 19:54:23 +0100

    Fix Vercel build: Remove unused actions and components

--- src/env.mjs
+++ src/env.mjs
@@ -3,32 +3,13 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().optional(),
     APP_URL: z.string().optional(),
     GOOGLE_SITE_VERIFICATION_ID: z.string().optional(),
-    GITHUB_ID: z.string().optional(),
-    GITHUB_SECRET: z.string().optional(),
-    NEXTAUTH_URL: z.string().optional(),
-    NEXTAUTH_SECRET: z.string().optional(),
-    STRIPE_SECRET_KEY: z.string().optional(),
-    STRIPE_WEBHOOK_SECRET_KEY: z.string().optional(),
-    STRIPE_SUBSCRIPTION_PRICE_ID: z.string().optional(),
   },
   client: {
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().optional(),
   },
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     APP_URL: process.env.APP_URL,
     GOOGLE_SITE_VERIFICATION_ID: process.env.GOOGLE_SITE_VERIFICATION_ID,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET_KEY: process.env.STRIPE_WEBHOOK_SECRET_KEY,
-    STRIPE_SUBSCRIPTION_PRICE_ID: process.env.STRIPE_SUBSCRIPTION_PRICE_ID,
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   },
 });
